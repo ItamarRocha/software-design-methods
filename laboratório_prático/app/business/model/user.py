f6724f691ba868b967762c9ca1cdfb3a854d7013
jpvt <joaoteixeira@eng.ci.ufpb.br>
2020-11-18 23:31:25 -0300

    Add: User Class

---
+++ laboratório_prático/app/business/model/user.py
@@ -0,0 +1,24 @@
+class User:
+
+    def __init__(self, login, password):
+
+        self.__login = login
+        self.__password = password
+
+    
+    def getLogin():
+
+        return self.__login
+    
+    def getPassword():
+
+        return self.__login
+
+    def setLogin(login):
+
+        self.__login = login;
+
+    def setPassword(password):
+
+        self.__password = password
+
