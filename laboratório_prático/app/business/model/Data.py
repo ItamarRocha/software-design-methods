b30506c9b8a96d0c64ba784735ce26b4664a61f1
SAndradeTC <satc1624@gmail.com>
2020-11-20 18:20:18 -0300

    classe data

---
+++ laboratório_prático/app/business/model/Data.py
@@ -0,0 +1,31 @@
+class Data:
+
+    def __init__(self, dd, mm, yyyy):
+
+        self.__dd = dd
+        self.__mm = mm
+        self.__yyyy = yyyy
+
+    def getDD(self):
+
+        return self.__dd
+    
+    def getMM(self):
+
+        return self.__mm
+
+    def getYYYY(self):
+
+        return self.__yyyy
+
+    def setDD(self,dd):
+
+        self.__dd = dd
+
+    def setMM(self,mm):
+
+        self.__mm = mm
+
+    def setYYYY(self,yyyy):
+
+    self.__yyyy= yyyy
