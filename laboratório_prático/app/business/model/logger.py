404c93123298a967265b17f12761eed652f555eb
joallace <wallace.lucena@hotmail.com>
2020-12-01 18:33:03 -0300

    Alterada estrutura do logger

--- laboratório_prático/app/business/model/logger.py
+++ laboratório_prático/app/business/model/logger.py
@@ -1,12 +1,16 @@
 from abc import ABC, abstractmethod
 import datetime
+import json
 
 class Logger(ABC):
     def __init__(self):
-        self.__log_str = f"Log from {datetime.datetime.now().strftime("%d/%m/%Y_%H:%M:%S")}"
+        self.__data = {"datetime": datetime.datetime.now().strftime("%d/%m/%Y_%H:%M:%S"), "users_accesses": []}
 
     def logToFile(self):
         self.convert2Format()
+
+    def writeAcess(self, user_name):
+        self.__data["users_accesses"].append({"username": user_name, "acess_time": datetime.datetime.now().strftime("%d/%m/%Y_%H:%M:%S")})
 
     @abstractmethod
     def convert2Format(self) -> None:
@@ -14,8 +18,15 @@ class Logger(ABC):
 
     def saveFile(self, path):
         with open(path, "w") as f:
-            
+            f.write(self.convert2Format())
+
+        f.close()
 
 
 
 class PDFLogger(Logger):
+    def convert2Format(self):
+        return json.dump()
+
+class HTMLLogger(Logger):
+    
