f1dd346c78cf7be66b10b1fea0db527ed8dca3c6
joallace <wallace.lucena@hotmail.com>
2020-12-01 23:48:20 -0300

    Finalizado o logger

--- laboratório_prático/app/business/model/logger.py
+++ laboratório_prático/app/business/model/logger.py
@@ -5,6 +5,7 @@ import json
 class Logger(ABC):
     def __init__(self):
         self.__data = {"datetime": datetime.datetime.now().strftime("%d/%m/%Y_%H:%M:%S"), "users_accesses": []}
+        self.__format = ".bin"
 
     def logToFile(self):
         self.convert2Format()
@@ -16,17 +17,44 @@ class Logger(ABC):
     def convert2Format(self) -> None:
         pass
 
-    def saveFile(self, path):
-        with open(path, "w") as f:
+    def saveFile(self):
+        with open(f"{self.__data['datetime']}.{self.__format}", "w") as f:
             f.write(self.convert2Format())
 
         f.close()
 
+class JSONLogger(Logger):
+    def __init__(self):
+        Logger.__init__(self)
+        self.__format = ".json"
 
-
-class PDFLogger(Logger):
     def convert2Format(self):
-        return json.dump()
+        return json.dumps(self.__data)
 
-class HTMLLogger(Logger):
-    
+class TXTLogger(Logger):
+    def __init__(self):
+        Logger.__init__(self)
+        self.__format = ".txt"
+
+    def convert2Format(self):
+        txt = f"---==== Log from {self.__data['datetime']} ====---\n\n"
+
+        txt += "\tusername\t\tacess_time\n\n"
+
+        for user in self.__data["users_accesses"]:
+            txt += f"{user['username']}\t\t{user['acess_time']}\n"
+
+        return txt
+
+class CSVLogger(Logger):
+    def __init__(self):
+        Logger.__init__(self)
+        self.__format = ".csv"
+
+    def convert2Format(self):
+        csv = "username,acess_time\n"
+
+        for user in self.__data["users_accesses"]:
+            csv += f"{user['username']},{user['acess_time']}\n"
+
+        return csv
