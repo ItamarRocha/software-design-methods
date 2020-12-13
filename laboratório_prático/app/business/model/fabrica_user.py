f6aa50675398a4623eb579843d3d633bd8b3c987
claudiosouzabrito <36930457+claudiosouzabrito@users.noreply.github.com>
2020-12-13 18:01:59 -0300

    Atualizando fábrica de usuários

--- laboratório_prático/app/business/model/fabrica_user.py
+++ laboratório_prático/app/business/model/fabrica_user.py
@@ -1,6 +1,7 @@
+from business.model.empreendimento import Empreendimento
 from ..model.user import User
 from ..model.data import Data
 
 class FabricaUser:
-    def criaUser(self, login, password, dataNascimento):
-        return User(login, password, dataNascimento) 
+    def criaUser(self, login: str = "", password: str = "", dataNascimento: str = "", empreendimento=Empreendimento()):
+        return User(login, password, dataNascimento, empreendimento) 
