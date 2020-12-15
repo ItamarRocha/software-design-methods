709dec093e4612b445bf4b25ae2a48f232000b0a
Sarah Andrade Toscano de Carvalho <33987228+SarahToscano@users.noreply.github.com>
2020-12-14 23:51:40 -0300

    Update fabrica_user.py

--- laboratório_prático/app/business/model/fabrica_user.py
+++ laboratório_prático/app/business/model/fabrica_user.py
@@ -2,6 +2,7 @@ from business.model.empreendimento import Empreendimento
 from ..model.user import User
 from ..model.data import Data
 
+#Cria uma classe que vai ser acessada pelas outras com argumentos gerais
 class FabricaUser:
     def criaUser(self, login: str = "", password: str = "", dataNascimento: str = "", empreendimento=Empreendimento()):
         return User(login, password, dataNascimento, empreendimento) 
